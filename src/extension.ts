a2a6a57e77617b469f131795dd5f6a7bc6e5f86f
k15 <krisnantobiyuh@gmail.com>
2023-06-21 09:43:46 +0700

    feat: :sparkles: register command to extension

--- src/extension.ts
+++ src/extension.ts
@@ -1,6 +1,7 @@
 // The module 'vscode' contains the VS Code extensibility API
 // Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+import calculateCommands from './commands/calculateCommand';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,14 +14,11 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('codemath.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from CodeMath!');
-	});
+  calculateCommands();
 
-	context.subscriptions.push(disposable);
 }
 
 // This method is called when your extension is deactivated
-export function deactivate() {}
+export function deactivate() {
+  // Code here
+}
